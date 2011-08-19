fcacb145d3e8b28dfff84384df7f7508e02dca4b
Stuart Dallas <github@3ft9.com>
2011-08-19 19:15:55 +0100

    Implemented a mock ApiClient object for testing purposes.

--- test/helper.rb
+++ test/helper.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'test/unit'
 require 'shoulda'
 require 'yaml'
+require File.dirname(__FILE__) + '/../lib/DataSift/mockapiclient'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
@@ -11,5 +12,13 @@ class Test::Unit::TestCase
 	def init()
 		@config = YAML::load(File.open(File.join(File.dirname(__FILE__), '..', 'config.yml')))
 		@testdata = YAML::load(File.open(File.join(File.dirname(__FILE__), 'testdata.yml')))
+	end
+
+	def initUser(mock = true)
+			@user = DataSift::User.new(@config['username'], @config['api_key'])
+			if mock
+				@user.setApiClient(DataSift::MockApiClient.new())
+				@user.api_client.clearResponse()
+			end
 	end
 end
