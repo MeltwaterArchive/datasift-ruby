019c0ed8a59703234f92996023edd211f81a5500
Stuart Dallas <github@3ft9.com>
2011-11-08 11:07:56 +0000

    Added the User.getUsage method.

--- test/test_user.rb
+++ test/test_user.rb
@@ -16,6 +16,28 @@ class TestUser < Test::Unit::TestCase
 			assert_not_nil @user
 			assert_equal @config['api_key'], @user.api_key
 		end
+
+		should "return valid summary usage information" do
+			@user.api_client.setResponse(200, {
+				'start' => 'Mon, 07 Nov 2011 14:20:00 +0000',
+				'streams' => {
+					'5e82aa9ac3dcf4dec1cce08a0cec914a' => {
+						'seconds' => 313,
+						'licenses' => {
+							'twitter' => 17,
+							'facebook' => 5
+						}
+					}
+				},
+				'end' => 'Mon, 07 Nov 2011 15:20:00 +0000'
+			}, 200, 150)
+			usage = @user.getUsage()
+			assert_equal "Mon, 07 Nov 2011 14:20:00 +0000", usage['start']
+			assert_equal "Mon, 07 Nov 2011 15:20:00 +0000", usage['end']
+			assert_equal 313, usage['streams']['5e82aa9ac3dcf4dec1cce08a0cec914a']['seconds']
+			assert_equal 17, usage['streams']['5e82aa9ac3dcf4dec1cce08a0cec914a']['licenses']['twitter']
+			assert_equal 5, usage['streams']['5e82aa9ac3dcf4dec1cce08a0cec914a']['licenses']['facebook']
+		end
 	end
 
 	context "Given an empty definition from the User factory" do
@@ -63,6 +85,33 @@ class TestUser < Test::Unit::TestCase
 
 		should "have a rate limit remaining value" do
 			assert @user.rate_limit_remaining == 150
+		end
+	end
+
+	context "The exception raised by the User.getUsage method" do
+		setup do
+			init()
+			initUser()
+		end
+
+		should "be APIError given a 400 response" do
+			@user.api_client.setResponse(400, { 'error' => 'Bad request from user supplied data'}, 200, 150)
+			assert_raise(DataSift::APIError) { usage = @user.getUsage() }
+		end
+
+		should "be AccessDenied given a 401 response" do
+			@user.api_client.setResponse(401, { 'error' => 'User banned because they are a very bad person'}, 200, 150)
+			assert_raise(DataSift::AccessDeniedError) { usage = @user.getUsage() }
+		end
+
+		should "be APIError given a 404 response" do
+			@user.api_client.setResponse(404, { 'error' => 'Endpoint or data not found'}, 200, 150)
+			assert_raise(DataSift::APIError) { usage = @user.getUsage() }
+		end
+
+		should "be APIError given a 500 response" do
+			@user.api_client.setResponse(500, { 'error' => 'Problem with an internal service'}, 200, 150)
+			assert_raise(DataSift::APIError) { usage = @user.getUsage() }
 		end
 	end
 end
