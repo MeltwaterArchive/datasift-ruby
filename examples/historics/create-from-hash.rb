83081ab3f78fc022949d82e16b97bc78f88bbe2a
dugjason <jason.dugdale@gmail.com>
2012-12-04 11:41:27 +0000

    Corrected examples: name and sample params were being passed to createHistoric() method in the wrong order

--- examples/historics/create-from-hash.rb
+++ examples/historics/create-from-hash.rb
@@ -11,18 +11,18 @@ def usage(message = '', end_of_story = true)
 	puts message + '\n' unless message.length() == 0
 	puts
 	puts 'Usage: create-from-hash \\'
-	puts '            <username> <api_key> <hash> <start> <end> <sources> <name> <sample>'
+	puts '            <username> <api_key> <hash> <start> <end> <sources> <sample> <name>'
 	puts
 	puts 'Where: hash    = the stream hash the query should run'
 	puts '       start   = the start date for the query (YYYYMMDDHHMMSS)'
 	puts '       end     = the end date for the query (YYYYMMDDHHMMSS)'
 	puts '       sources = comma separated list of data sources (e.g. twitter)'
-	puts '       name    = a friendly name for the query'
 	puts '       sample  = the sample rate'
+	puts '       name    = a friendly name for the query'
 	puts
 	puts 'Example'
 	puts '       create-from-hash <hash> 20120101000000 20120101235959 \\'
-	puts '                      twitter HistoricsQuery123 100'
+	puts '                      twitter 100 HistoricsQuery123'
 	puts
 	exit 1 unless not end_of_story
 end
@@ -42,8 +42,8 @@ stream_hash = env.args[0]
 start_date  = env.args[1]
 end_date    = env.args[2]
 sources     = env.args[3].split(',')
-name        = env.args[4]
-sample      = env.args[5]
+sample      = env.args[4]
+name        = env.args[5]
 
 # Parse the dates
 start_date = DateTime.strptime(start_date, '%Y%m%d%H%M%S')
@@ -51,7 +51,7 @@ end_date   = DateTime.strptime(end_date,   '%Y%m%d%H%M%S')
 
 begin
 	# Create the Historics query
-	historic = env.user.createHistoric(stream_hash, start_date, end_date, sources, name, sample)
+	historic = env.user.createHistoric(stream_hash, start_date, end_date, sources, sample, name)
 
 	# Prepare the query
 	historic.prepare()
