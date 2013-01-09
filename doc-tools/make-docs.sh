f206a993ce7292249b2a22b8b5610bd5959f89ca
Jacek Artymiak <jacek.artymiak@datasift.com>
2013-01-09 11:16:34 +0000

    Switched build directory to /tmp.

--- doc-tools/make-docs.sh
+++ doc-tools/make-docs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # -v
 
-export BASE_DIR="$( cd "$( dirname $0 )/../../../.." && pwd )/"
+export BASE_DIR="`pwd`/"
 
 source ${BASE_DIR}ms-tools/doc-tools/docathon/sub/make-docs-util-defs.sh
+export BASE_DIR="/tmp/$(basename $0).$$.tmp/"
 initialise $*
 
 ### Ruby-specific parameters
