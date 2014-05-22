2485f566ee06d88a2c82c8e2516d10e13543bbe1
Jason Dugdale <jason.dugdale@gmail.com>
2014-05-22 12:01:26 +0100

    Rewrote test suite using MiniTest

---
+++ test/test_helper.rb
@@ -0,0 +1,18 @@
+require 'simplecov'
+SimpleCov.start
+
+require File.expand_path('./../../examples/auth', __FILE__)
+require 'datasift'
+#require 'minitest/spec'
+require 'minitest/autorun'
+require 'webmock/minitest'
+require 'multi_json'
+require 'ostruct'
+
+def fixture_path
+  File.expand_path('../fixtures', __FILE__)
+end
+
+def fixture(file)
+  File.new(fixture_path + '/' + file)
+end
