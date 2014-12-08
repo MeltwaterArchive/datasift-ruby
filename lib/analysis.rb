244659bcf236fab4a5f3e352b111c0a6b67100d9
Jason Dugdale <jason.dugdale@gmail.com>
2014-12-08 17:10:32 +0000

    Minor changes to Pylon API endpoints

--- lib/analysis.rb
+++ lib/analysis.rb
@@ -1,10 +1,11 @@
 module DataSift
   class Analysis < DataSift::ApiResource
 
-    def validate(csdl)
+    def valid?(csdl, boolResponse = true)
       params = {:csdl => csdl}
       requires params
-      DataSift.request(:POST, 'analysis/validate', @config, params)
+      res = DataSift.request(:POST, 'analysis/validate', @config, params)
+      boolResponse ? res[:http][:status] == 200 : res
     end
 
     def compile(csdl)
@@ -15,32 +16,38 @@ module DataSift
 
     def get(hash = '')
       params = {:hash => hash}
-      requires params
       DataSift.request(:GET, 'analysis/get', @config, params)
     end
 
-    def start(hash)
-      params = {:hash => hash}
+    def start(hash, name)
+      params = {
+        :hash => hash,
+        :name => name
+      }
       requires params
-      DataSift.request(:PUT, 'analysis/start', @config, params)
+      DataSift.request(:POST, 'analysis/start', @config, params)
     end
 
-    def stop(csdl)
+    def stop(hash)
       params = {:hash => hash}
       requires params
-      DataSift.request(:PUT, 'analysis/stop', @config, params)
+      DataSift.request(:POST, 'analysis/stop', @config, params)
     end
 
-    def analyze(hash, parameters, filter = '', start = '', end = '', include_parameters_in_reply = false)
+    def analyze(hash, parameters, filter = '', start_time = '', end_time = '', include_parameters_in_reply = false)
       params = {
         :hash                         => hash,
         :parameters                   => parameters,
-        :filter                       => filter,
-        :start                        => start,
-        :end                          => end,
-        :include_parameters_in_reply  => include_parameters_in_reply
       }
       requires params
+
+      optional_params = {
+        :filter                       => filter,
+        :start                        => start_time,
+        :end                          => end_time,
+        :include_parameters_in_reply  => include_parameters_in_reply
+      }
+      params.merge! optional_params
       DataSift.request(:POST, 'analysis/analyze', @config, params)
     end
 
