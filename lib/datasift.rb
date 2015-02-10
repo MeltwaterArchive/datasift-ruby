dd8ec49394bdf22e38fe76774a82d858f6eb433e
Jason Dugdale <jason.dugdale@gmail.com>
2015-02-10 12:11:11 +0000

    Use TLSv1.2 by default and move SSL specification to config

--- lib/datasift.rb
+++ lib/datasift.rb
@@ -46,11 +46,6 @@ module DataSift
     #+config+:: A hash containing configuration options for the client for e.g.
     # {username => 'some_user', api_key => 'ds_api_key', 'enable_ssl' => true, open_timeout => 30, timeout => 30}
     def initialize (config)
-      #only SSLv3 and TLSv1 currently supported, TLSv1 preferred
-      # this is fixed in REST client and is scheduled for the 1.7.0 release
-      # see https://github.com/rest-client/rest-client/pull/123
-      OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ssl_version] = 'TLSv1'
-
       if config == nil
         raise InvalidConfigError.new ('Config cannot be nil')
       end
@@ -156,7 +151,8 @@ module DataSift
       :timeout      => timeout,
       :payload      => payload,
       :url          => url,
-      :ssl_version  => OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ssl_version]
+      :ssl_version  => OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ssl_version],
+      :verify_ssl   => OpenSSL::SSL::VERIFY_PEER
     )
 
     begin
