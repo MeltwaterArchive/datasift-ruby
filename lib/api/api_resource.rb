eb2ecc6e1a65dd6e61b004773fbd193594125882
Jason Dugdale <jason.dugdale@gmail.com>
2015-02-23 17:03:50 +0000

    Hash#has_key? is deprecated in favor of Hash#key?

--- lib/api/api_resource.rb
+++ lib/api/api_resource.rb
@@ -2,18 +2,18 @@ module DataSift
   class ApiResource
     include DataSift
 
-    def initialize (config)
+    def initialize(config)
       @config = config
-      config[:api_host] = 'api.datasift.com' unless config.has_key?(:api_host)
-      config[:stream_host] = 'websocket.datasift.com' unless config.has_key?(:stream_host)
-      config[:api_version] = 'v1' unless config.has_key?(:api_version)
-      config[:enable_ssl] = false unless config.has_key?(:enable_ssl)
+      config[:api_host] = 'api.datasift.com' unless config.key?(:api_host)
+      config[:stream_host] = 'websocket.datasift.com' unless config.key?(:stream_host)
+      config[:api_version] = 'v1' unless config.key?(:api_version)
+      config[:enable_ssl] = false unless config.key?(:enable_ssl)
       # max 320 seconds retry - http://dev.datasift.com/docs/streaming-api/reconnecting
-      config[:max_retry_time] = 320 unless config.has_key?(:max_retry_time)
-      config[:retry_timeout] = 0 unless config.has_key?(:retry_timeout)
+      config[:max_retry_time] = 320 unless config.key?(:max_retry_time)
+      config[:retry_timeout] = 0 unless config.key?(:retry_timeout)
     end
 
-    def requires params
+    def requires(params)
       params.each { |k, v|
         if v == nil || v.to_s.length == 0
           raise InvalidParamError.new "#{k} is a required parameter, it cannot be nil or empty"
