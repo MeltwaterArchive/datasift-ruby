58c018298b5f5b4434b4bb8b2a56c8b42f1c425f
Jason Dugdale <jason.dugdale@gmail.com>
2015-04-21 15:37:49 +0100

    Do not pass unnecessary params param

--- lib/account_identity_token.rb
+++ lib/account_identity_token.rb
@@ -83,7 +83,7 @@ module DataSift
       fail BadParametersError, 'identity_id is required' if identity_id.empty?
       fail BadParametersError, 'service is required' if service.empty?
 
-      DataSift.request(:DELETE, "account/identity/#{identity_id}/token/#{service}", @config, params)
+      DataSift.request(:DELETE, "account/identity/#{identity_id}/token/#{service}", @config)
     end
   end
 end
