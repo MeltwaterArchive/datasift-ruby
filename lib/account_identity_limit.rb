840fafc9e6e276cbf0aec2e6e94781aa7b1d405e
Jason Dugdale <jason.dugdale@gmail.com>
2015-04-28 13:01:45 +0100

    Resolves DQ-680. Removed unnecessary params param

--- lib/account_identity_limit.rb
+++ lib/account_identity_limit.rb
@@ -77,7 +77,7 @@ module DataSift
       fail BadParametersError, 'identity_id is required' if identity_id.empty?
       fail BadParametersError, 'service is required' if service.empty?
 
-      DataSift.request(:DELETE, "account/identity/#{identity_id}/limit/#{service}", @config, params)
+      DataSift.request(:DELETE, "account/identity/#{identity_id}/limit/#{service}", @config)
     end
   end
 end
