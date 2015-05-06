c403e26552a0339faea84a9c97f6e6b791cbb612
Jason Dugdale <jason.dugdale@gmail.com>
2015-05-06 11:22:53 +0100

    DEV-2159; add support for historics_id param for /dpu endpoint

--- test/datasift/core_api_test.rb
+++ test/datasift/core_api_test.rb
@@ -117,6 +117,41 @@ describe 'DataSift' do
         end
       end
     end
+
+    it 'requires_at_least_one_param' do
+      assert_raises ArgumentError do
+        @datasift.dpu
+      end
+    end
+  end
+
+  describe '#dpu for Historics' do
+    before do
+      VCR.use_cassette('core/before_historic_dpu') do
+        @hash = @datasift.compile(@data.valid_csdl)[:data][:hash]
+        @historic = @datasift.historics.prepare(
+          @hash,
+          Time.now.to_i - 10800,
+          Time.now.to_i - 7200,
+          'Ruby test suite',
+          'tumblr',
+          10
+        )
+      end
+    end
+
+    after do
+      VCR.use_cassette('core/after_historic_dpu') do
+        @datasift.historics.delete @historic[:data][:id]
+      end
+    end
+
+    it 'can_get_dpu_cost_for_historic' do
+      VCR.use_cassette('core/historic_dpu') do
+        response = @datasift.dpu('', @historic[:data][:id])
+        assert_equal STATUS.valid, response[:http][:status]
+      end
+    end
   end
 
   ##
