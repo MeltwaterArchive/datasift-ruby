335b19aefce9732720fec9e3bb0041833ddee047
Duccio Giovannelli <giovannelli@extendi.it>
2015-06-15 13:52:14 +0200

    Api resource removed ssl_default

--- lib/api/api_resource.rb
+++ lib/api/api_resource.rb
@@ -17,12 +17,12 @@ module DataSift
       #   this is fixed in REST client and is scheduled for the 1.7.0 release
       #   see https://github.com/rest-client/rest-client/pull/123
 
-      ssl_default = "TLSv1_2"
-      if RUBY_VERSION.to_i == 1
-        ssl_default = "TLSv1"
-      end
-      OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ssl_version] = config[:ssl_version] ||
-        ssl_default
+      # ssl_default = "TLSv1_2"
+      # if RUBY_VERSION.to_i == 1
+      #   ssl_default = "TLSv1"
+      # end
+      # OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ssl_version] = config[:ssl_version] ||
+      #   ssl_default
 
       # max 320 seconds retry - http://dev.datasift.com/docs/streaming-api/reconnecting
       config[:max_retry_time] = 320 unless config.key?(:max_retry_time)
