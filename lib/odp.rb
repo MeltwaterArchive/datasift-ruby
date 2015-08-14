8ec44e10e7c4e31d269b28a9adbc08a35154607d
Jason Dugdale <jason.dugdale@gmail.com>
2015-08-14 17:00:20 +0100

    Fail if user fails to pass correct params to odp.ingest method

--- lib/odp.rb
+++ lib/odp.rb
@@ -4,6 +4,9 @@ module DataSift
   #   DataSift for processing
   class Odp < DataSift::ApiResource
     def ingest(source_id, data)
+      fail ArgumentError, 'source_id is required' if source_id.nil?
+      fail ArgumentError, 'data payload is required' if data.nil?
+
       config = @config.merge(api_host: @config[:ingestion_host], api_version: nil)
       DataSift.request(:POST, source_id, config, data)
     end
