7830043db6f0746802b522d1b5d969b64cacd769
Jason Dugdale <jason.dugdale@gmail.com>
2015-08-14 16:03:39 +0100

    Fix double slash in ODP batch upload URL

--- lib/odp.rb
+++ lib/odp.rb
@@ -5,7 +5,7 @@ module DataSift
   class Odp < DataSift::ApiResource
     def ingest(source_id, data)
       config = @config.merge(api_host: @config[:ingestion_host], api_version: nil)
-      DataSift.request(:POST, "/#{source_id}", config, data)
+      DataSift.request(:POST, source_id, config, data)
     end
   end
 end
