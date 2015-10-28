a26c3a9b664a18057021133f4a3828f6ac914e71
Jason Dugdale <jason.dugdale@gmail.com>
2015-10-28 10:44:08 +0000

    Fixed typo in docs

--- lib/account.rb
+++ lib/account.rb
@@ -4,7 +4,7 @@ module DataSift
   class Account < DataSift::ApiResource
     # Check your account usage for a given period and timeframe
     #
-    # @param period [String] (Optional) Period is one of either daily, weekly or monthly
+    # @param period [String] (Optional) Period is one of either hourly, daily or monthly
     # @param start_time [Integer] (Optional) Unix timestamp of the start of the period
     #   you are querying
     # @param end_time [Integer] (Optional) Unix timestamp of the end of the period
